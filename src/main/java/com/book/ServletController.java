464caf111f60ebae3f20aab920007a4163afe3bc
Anfal Fadhil <AFadhil2@slb.com>
2022-09-28 12:16:35 -0500

    test

---
+++ src/main/java/com/book/ServletController.java
@@ -0,0 +1,24 @@
+package com.book;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ServletController", value = "/ServletController")
+public class ServletController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter output = response.getWriter();
+        output.println("Welcome to ControllerServlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
